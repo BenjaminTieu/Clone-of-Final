4685bad660121057c4caf99b50ffb6aabad71e1a
Benjamin Tieu <betieu@calpoly.edu>
2025-02-24 22:56:09 -0800

    changed user_input to list the choices along with an associated number and edited main.py a little

--- CS-Final-Project/main.py
+++ CS-Final-Project/main.py
@@ -3,13 +3,6 @@ import character_creation
 import time
 import func
 
-# This function will convert a string into a float if possible. If it is not possible, it will return None
-def str_to_float(phr1: str) -> float or None:
-    try:
-        return float(phr1)
-    except ValueError:
-        return None
-
 # Start Message
 print("--------------------------")
 print("Welcome to Text-Based RPG!")
@@ -19,25 +12,12 @@ time.sleep(.5)
 # Initialize Character Parameters
 # Prompts the user to choose a name for the character
 name  = input("Enter Your Character's Name: \n")
+# Confirmation for name
+print("\n", end="")
+func.user_input("Are you sure you want to name your character {}?".format(name), ["yes", "no"])
 
 # Prompts the user to choose an archetype
-print("\nChoose your archetype: ")
-i = 0
-archetype_list = []
-archetype_dict = {}
-for key in archetypes.dict1:
-    i += 1
-    print("[{}] {}".format(i, key))
-    archetype_list.extend([key, i])
-    archetype_dict[key] = i
-archetype = func.user_input("Enter the name or number associated with the archetype: ", archetype_list)
-for key in archetype_dict:
-    if str_to_float(archetype) is not None:
-        if str_to_float(archetype) == archetype_dict[key]:
-            archetype = key
-    else:
-        if archetype.lower() == key.lower():
-            archetype = key
+archetype = func.user_input("Choose your archetype", ["Warrior", "Thief"])
 func.prompt1()
 character1 = character_creation.Character(name, archetypes.Archetypes(archetype))
 print(character1)
