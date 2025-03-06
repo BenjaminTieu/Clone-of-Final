ce4b4c660d765b7d27792b7885fe6f99b2a0702e
BenjaminTieu <betieu@calpoly.edu>
2025-03-05 17:50:42 -0800

    Add files via upload

---
+++ CS-Final-Project/stor.py
@@ -0,0 +1,14 @@
+# This dictionary stores a verification phrase with "yes" and "no" options
+phr_verification = ["Yes", "No"]
+
+# This dictionary stores the names of all the archetypes
+archetype_names = ["Warrior", "Thief"]
+
+# This dictionary stores the attributes of all the archetypes
+archetype_dict = {"Warrior": {"Archetype": "Warrior",
+                      "Description": "A brawler archetype with a focus on getting up-close and personal",
+                      "Health": 10, "Strength": 10, "Dexterity": 5, "Agility": 5},
+                  "Thief": {"Archetype": "Thief",
+                      "Description": "A sneaky and nimble class that runs circles around its enemies",
+                      "Health": 5, "Strength": 5, "Dexterity": 10, "Agility": 10}}
+
